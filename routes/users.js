f9e44ea07351421b0c28a16836ac2e915c3423b3
zocalop <jcb2861@gmail.com>
2026-07-08 13:19:52 -0300

    Updated users.js in main

--- routes/users.js
+++ routes/users.js
@@ -55,7 +55,21 @@ router.post("/",(req,res)=>{
 // PUT request: Update the details of a user by email ID
 router.put("/:email", (req, res) => {
   // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  const email = req.params.email;
+  let filtered_users = users.filter((user) => user.email === email);
+
+  if (filtered_users.length > 0) {
+    let filtered_user = filtered_users[0];
+    let DOB = req.query.DOB;
+    if (DOB) {
+      filtered_user.DOB = DOB;
+    }
+    users = users.filter((user) => user.email != email);
+    users.push(filtered_user);
+    res.send(`User with email ${email} updated.`);  //This line is to be replaced with actual return value
+  } else {
+    res.send("Unable to find user!");
+  }
 });
 
 
